b44df1cd33246fd1201b05a62ac75ce99d282b74
Pierre-Alain Moret <moret.pierre.alain@gmail.com>
2023-03-02 09:50:14 +0100

    State pattern

--- js/apiState/index.js
+++ js/apiState/index.js
@@ -9,7 +9,17 @@ class Context {
     }
 
     change(status) {
-    }
+        const [
+            InitialState,
+            ErrorState,
+            SuccessState
+        ] = this._states
+ 
+        if (status === 'error') {
+            this.currentState = ErrorState
+        } else if (status === 'success') {
+            this.currentState = SuccessState
+        }    }
 
     get state() {
         return this.currentState.state
